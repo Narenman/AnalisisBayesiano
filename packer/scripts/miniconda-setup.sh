e9645d5200347782b047603f41c9225a89df5767
David A. Barajas-Solano <David.Barajas-Solano@pnnl.gov>
2019-08-20 18:45:37 -0700

    Fixing provisioning

--- packer/scripts/miniconda-setup.sh
+++ packer/scripts/miniconda-setup.sh
@@ -11,7 +11,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O mi
 bash ~/miniconda.sh -b -p $HOME/miniconda
 
 # Set up the environment
-~/miniconda/bin/conda create -n u18fest_env numpy=1.15.* gxx_linux-64
+~/miniconda/bin/conda create -n u18fest_env numpy=1.15.* gxx_linux-64 pymc3 jupyter
 
 # Configure Jupyter
 ~/miniconda/envs/u18fest_env/jupyter notebook --generate-config
@@ -19,4 +19,25 @@ echo "c.NotebookApp.token = ''" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.password = ''" >> ~/.jupyter/jupyter_notebook_config.py
 
 # Wrap up
+BANNER_MSG="**************************************************\n
+Jupyter VM for U18 Fest
+
+Your login is u18fest and your password is u18fest 
+
+jupyter-start = Start Jupyter server
+jupyter-stop  = Stop Jupyter server
+shutdown      = Shut down the VM
+
+**************************************************\n"
+
+echo -e "${BANNER_MSG}" | sudo tee /etc/issue     > /dev/null
+echo -e "${BANNER_MSG}" | sudo tee /etc/issue.net > /dev/null
+
+echo "alias shutdown='sudo shutdown -h 0'" >> ~/.bashrc
+echo "alias jupyter-stop='~/miniconda/envs/u18fest_env/bin/jupyter notebook stop 9000'" >> ~/.bashrc
+echo "jupyter-start () {
+cd ~/u18fest/notebooks
+~/miniconda/envs/u18fest_env/bin/jupyter notebook --port=9000 --no-browser --ip=0.0.0.0 &
+}"  >> ~/.bashrc
+
 sync
